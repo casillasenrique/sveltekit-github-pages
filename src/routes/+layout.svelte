3f69791fee3eed2963d95e2c23b5a096b570650b
enrique <casillasenrique@live.com>
2024-02-26 00:15:03 -0500

    Revert

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,12 +7,12 @@
 
 <nav>
   <ul>
-    <li><Link href="/">Home</Link></li>
+    <!-- <li><Link href="/">Home</Link></li>
     <li><Link href="/resume">Resume</Link></li>
-    <li><Link href="/projects">Projects</Link></li>
-    <!-- <a href="{base}/">Home</a>
+    <li><Link href="/projects">Projects</Link></li> -->
+    <a href="{base}/">Home</a>
     <a href="{base}/resume">Resume</a>
-    <a href="{base}/projects">Projects</a> -->
+    <a href="{base}/projects">Projects</a>
   </ul>
 </nav>
 
