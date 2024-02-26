1351821f40c13bf92ec1d468021e7fa6fd90e553
enrique <casillasenrique@live.com>
2024-02-26 10:53:26 -0500

    Remove slash

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,4 +5,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <img src={bar} alt="" />
-<img src="/images/cayuga centers.png" alt="">
+<img src="images/cayuga centers.png" alt="">
