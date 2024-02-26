4dfcbaef537ba5e54d69495aaa6138f0589d502c
enrique <casillasenrique@live.com>
2024-02-26 10:52:12 -0500

    Test assets

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+  import bar from '$lib/assets/images/352-3524477_bar-chart-comments-bar-chart-icon-svg.png';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<img src={bar} alt="" />
+<img src="/images/cayuga centers.png" alt="">
