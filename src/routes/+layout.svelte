f5d232db672df58f440001fbae2e75731e2bb13e
enrique <casillasenrique@live.com>
2024-02-25 23:55:54 -0500

    Added home url to navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
-
-    import Link from "../lib/components/Link.svelte";
-
+  import Link from '../lib/components/Link.svelte';
+  import { base } from '$app/paths';
 </script>
+
 <nav>
   <ul>
     <!-- <li><Link href="/">Home</Link></li>
     <li><Link href="/resume">Resume</Link></li>
     <li><Link href="/projects">Projects</Link></li> -->
+    <a href="{base}/">Home</a>
   </ul>
 </nav>
 
