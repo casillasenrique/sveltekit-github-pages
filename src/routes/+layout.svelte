99131cc67866a07486398aef3d6239d2cb9047ce
enrique <casillasenrique@live.com>
2024-02-25 23:59:45 -0500

    Remaining links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
   import Link from '../lib/components/Link.svelte';
   import { base } from '$app/paths';
+
+  console.log(base);
 </script>
 
 <nav>
@@ -9,6 +11,8 @@
     <li><Link href="/resume">Resume</Link></li>
     <li><Link href="/projects">Projects</Link></li> -->
     <a href="{base}/">Home</a>
+    <a href="{base}/resume">Resume</a>
+    <a href="{base}/projects">Projects</a>
   </ul>
 </nav>
 
